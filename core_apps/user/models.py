e6b28034ba2e5e5ad793204aa3eb5cde1cfbe3fd
Majholl <mohammadreza1231380@gmail.com>
2025-05-05 23:09:21 +0330

    added fields to users

--- core_apps/user/models.py
+++ core_apps/user/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.conf import settings
 from django.db import models
 from os import path
 from time import time
-
 
 from .manager import customUserManager
 from ..musicapp.models.playlists import PlaylistModel
@@ -31,6 +32,10 @@ class Users(AbstractUser):
         USER = 'user', 'User'
         ADMIN = 'admin', 'Admin'
         SUPERADMIN = 'superadmin', 'Superadmin'
+    
+    class AccountStatus(models.TextChoices):
+        ACTIVE = 'active', 'Active'
+        LOCKED = 'locked', 'Locked'
         
     email = models.EmailField(db_index=True, unique=True)
     playlists = models.ManyToManyField(to=PlaylistModel, blank=True, related_name='playlists_users')
@@ -38,7 +43,11 @@ class Users(AbstractUser):
     profile = models.ImageField(upload_to=profile_file_cover, blank=True)
     usertype = models.CharField(max_length=10, choices=userType.choices, default=userType.USER)
     
+    account_status = models.CharField(max_length=8, choices=AccountStatus.choices, default=AccountStatus.ACTIVE)
+    
     reset_password = models.CharField(max_length=6, null=True)
+    reset_password_expire_time = models.DateTimeField(null=True, blank=True)
+    reset_password_attempt = models.IntegerField(default=0)
     
     created_at = models.BigIntegerField(default=nowTimeStamp)
     updated_at = models.BigIntegerField(default=nowTimeStamp)
@@ -56,7 +65,11 @@ class Users(AbstractUser):
     def __str__(self):
         return f'{self.email} - {self.usertype}'
     
-    
+    def set_resest_password(self,  code:str):
+        self.reset_password = code
+        self.reset_password_expire_time = timezone.now() + settings.RESET_PASSWORD_EXPIRE_TIME
+        self.save()
+        
     def get_full_name(self):
         return super().get_full_name()
     
