276c36858a25f859ef2d6748042bfcd224006350
Majholl <mohammadreza1231380@gmail.com>
2025-05-15 00:41:50 +0330

    playlist model added in admin panel of django

--- core_apps/musicapp/admin.py
+++ core_apps/musicapp/admin.py
@@ -1,4 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
-#//TODO add fields of artists and musics for the admin page
+
+from .models.playlists import PlaylistModel
+from .models.musics import MusicModel
+
+
+class PlaylisySite(admin.ModelAdmin):
+    list_display = ['title', 'description', 'cover', 'totaltracks', 'public_playlist']
+    
+    list_display_links = ['title', 'description']
+    
+    list_per_page = 15
+    
+    search_fields = ['playlists__title', 'playlists__public_playlist']
+    
+    list_filter = ['title', 'public_playlist']
+    
+    readonly_fields = ('get_musics_data',)
+    
+    fieldsets = (
+            
+            ('Playlist info', {
+                'fields': ('title', 'description')
+            }),
+            
+            ('others', {
+                'fields': ('cover', 'totaltracks', 'public_playlist'),
+            }),
+            
+            ('musics', {
+                'fields': ('get_musics_data', 'music_id'),
+            }),
+            
+        )
+    def get_musics_data(self, obj):
+        return ','.join([f" {i.pk}-{i.title} " for i in obj.music_id.all()])
+    
+    get_musics_data.short_description = 'musics - data'
+    
+    
+admin.site.register(PlaylistModel, PlaylisySite)
+admin.site.register(MusicModel)
