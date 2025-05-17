d1c471ac2ea3ed0dc55745743b8c836b1114aad0
Majholl <mohammadreza1231380@gmail.com>
2025-05-17 20:54:00 +0330

    added music model into django admin panel

--- core_apps/musicapp/admin.py
+++ core_apps/musicapp/admin.py
@@ -5,7 +5,7 @@ from .models.playlists import PlaylistModel
 from .models.musics import MusicModel
 
 
-class PlaylisySite(admin.ModelAdmin):
+class PlaylistSite(admin.ModelAdmin):
     list_display = ['title', 'description', 'cover', 'totaltracks', 'public_playlist']
     
     list_display_links = ['title', 'description']
@@ -16,22 +16,23 @@ class PlaylisySite(admin.ModelAdmin):
     
     list_filter = ['title', 'public_playlist']
     
-    readonly_fields = ('get_musics_data',)
+    readonly_fields = ('get_musics_data', 'created_at', 'updated_at')
     
     fieldsets = (
             
             ('Playlist info', {
-                'fields': ('title', 'description')
+                'fields': ('title', 'description',)
             }),
             
-            ('others', {
-                'fields': ('cover', 'totaltracks', 'public_playlist'),
+            ('Others', {
+                'fields': ('cover', 'totaltracks', 'public_playlist',),
             }),
             
-            ('musics', {
-                'fields': ('get_musics_data', 'music_id'),
+            ('Musics', {
+                'fields': ('get_musics_data', 'music_id',),
             }),
             
+            ('DateTime', {'fields': ('created_at', 'updated_at',),}),
         )
     def get_musics_data(self, obj):
         return ','.join([f" {i.pk}-{i.title} " for i in obj.music_id.all()])
@@ -39,5 +40,39 @@ class PlaylisySite(admin.ModelAdmin):
     get_musics_data.short_description = 'musics - data'
     
     
-admin.site.register(PlaylistModel, PlaylisySite)
-admin.site.register(MusicModel)
+    
+    
+    
+class MusicSite(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    
+    list_display_links = list_display
+    
+    list_per_page = 15
+    
+    search_fields = ['musics__title', 'musics__artist_id__name']
+    
+    list_filter =  ['title', 'artist_id__name']
+    
+    readonly_fields = ('get_artists_data', 'created_at', 'updated_at',)
+    
+    fieldsets = (
+        ('Music info', {'fields': ('title', 'duration', 'lyrics',)},),
+        
+        ('File-Cover', {'fields': ('cover', 'file',),}),
+        
+        ('Others', {'fields': ('artist_id', 'get_artists_data',),}),
+        
+        ('DateTime', {'fields': ('created_at', 'updated_at',),}),   
+    )
+    
+    def get_artists_data(self, obj):
+        return ','.join([f" {i.pk}-{i.name} " for i in obj.artist_id.all()])
+    
+    get_artists_data.short_description = 'artist - data'
+    
+    
+    
+
+admin.site.register(PlaylistModel, PlaylistSite)
+admin.site.register(MusicModel, MusicSite)
