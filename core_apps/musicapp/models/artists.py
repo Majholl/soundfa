c27a216a8cf4f53c2da0c123a955e1a31f350104
Majholl <mohammadreza1231380@gmail.com>
2025-05-20 01:57:50 +0330

    Updated artist model fields

--- core_apps/musicapp/models/artists.py
+++ core_apps/musicapp/models/artists.py
@@ -1,38 +1,45 @@
 from django.db import models
 from time import time
 from os import path
+from loguru import logger
 
+# -------------------------------------------------------------------
+    ### Artist Model ###
 
-"""
-    - Model of the artists in MYSQL db
-"""
+# This model adds artists :
+# - Many-to-many relationship with artist, musics, albums , generes
+# - Timestamp fields for creation and last update
+
+# -------------------------------------------------------------------
 
 
 
 def artists_direcrory(instance , filename): 
     try:
         splitedName = path.splitext(filename) 
-        fileName = f'{instance.name}_{instance.updated_at}{splitedName[-1]}'
+        fileName = f'{instance.name}_{int(time())}{splitedName[-1]}'
         return path.join('artists', fileName)
     except Exception as err :
-        print(f'Error while editing artist img name : error {str(err)}')
+        logger.info(f'Error saving artist  image name | artist-id : {instance.pk} | {str(err)}')
+        print(f'Error saving artist image name | artist-id : {instance.pk} | {str(err)}')
 
-
-def nowTimeStamp():
-    return int(time())
 
 
 class ArtistsModel(models.Model):
     
-    name = models.CharField(max_length=32)
-    realname = models.CharField(max_length=64, null=True)
-    
-    bio = models.CharField(max_length=256, null=True)
-    image = models.ImageField(upload_to=artists_direcrory)
-    
-    created_at = models.BigIntegerField(default=nowTimeStamp)
-    updated_at = models.BigIntegerField(default=nowTimeStamp)
+    name = models.CharField('Artist name', max_length=32)
+    realname = models.CharField('Artist realname', max_length=64, null=True)
+    bio = models.CharField('Artist bio', max_length=256, null=True)
+    image = models.ImageField(verbose_name='Artist image', upload_to=artists_direcrory)
+    created_at = models.DateTimeField('Creatation datetime', auto_now_add=True)
+    updated_at = models.DateTimeField('Last modification', auto_now=True)
 
+    
     class Meta:
-        db_table = 'artists'
         verbose_name = 'Artist'
+        db_table = 'artists'
+        ordering = ['-name']
+
+
+    def __str__(self):
+        return f'{self.name}'
