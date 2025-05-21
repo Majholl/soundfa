ac979f48b44c4baadd5d1cd4e4966d2380fd91de
Majholl <mohammadreza1231380@gmail.com>
2025-05-21 23:21:29 +0330

    Updated album fields

--- core_apps/musicapp/models/albums.py
+++ core_apps/musicapp/models/albums.py
@@ -1,40 +1,56 @@
 from django.db import models
 from time import time
 from os import path
+from loguru import logger
+
 from .artists import ArtistsModel
 from .musics import MusicModel
 
 
-"""
-    - Model of the albums in MYSQL db
-"""
+# -------------------------------------------------------------------
+    ### Album Model ###
+
+# This model adds artists :
+# - Many-to-many relationship with artist, musics
+# - Timestamp fields for creation and last update
+
+# -------------------------------------------------------------------
+
+
 
 def album_file_cover(instance ,filename):
     try:
         splitedName = path.splitext(filename)
-        fileName = f'{instance.title}_{instance.updated_at}{splitedName[-1]}'
+        fileName = f'{instance.title}_{int(time())}{splitedName[-1]}'
         return path.join('albums', fileName)
     except Exception as err:
-        print(f'Error while editting album img name : error {str(err)}')
+        logger.info(f'Error saving artist  image name | artist-id : {instance.pk} | {str(err)}')
+        print(f'Error saving artist image name | artist-id : {instance.pk} | {str(err)}')
 
 
-def nowTimeStamp():
-    return int(time())
 
 
 class AlbumModel(models.Model):
     
-    title = models.CharField(max_length=32)
-    albumcover = models.FileField(upload_to=album_file_cover)
+    title = models.CharField('Album title', max_length=32)
+    cover = models.FileField(verbose_name='Album cover', upload_to=album_file_cover)
 
-    artist_id = models.ManyToManyField(to=ArtistsModel, blank=True)
-    music_id = models.ManyToManyField(to=MusicModel, blank=True)
+    artist_id = models.ManyToManyField(verbose_name='Artists ablbum', to=ArtistsModel, blank=True)
+    music_id = models.ManyToManyField(verbose_name='Musics ablbum',to=MusicModel, blank=True)
     
-    totaltracks = models.BigIntegerField(null=True, blank=True)
-    description = models.CharField(max_length=256, null=True)
-    
-    created_at = models.BigIntegerField(default=nowTimeStamp)
-    updated_at = models.BigIntegerField(default=nowTimeStamp)
+    totaltracks = models.BigIntegerField('Ablum totaltracks', null=True, blank=True)
+    description = models.CharField('Album description', max_length=256, null=True)
+    created_at = models.DateTimeField('Creatation datetime', auto_now_add=True)
+    updated_at = models.DateTimeField('Last modification', auto_now=True)
 
     class Meta :
+        verbose_name = 'Albums'
         db_table = 'albums'
+        ordering = ['-title']
+        
+    def count_totaltracks(self, count):
+        self.totaltracks = count
+        self.save()    
+        
+    def __str__(self):
+        return f'{self.title}'        
