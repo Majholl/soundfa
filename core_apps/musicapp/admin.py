5e5f5d0d77f9145462727c49370af075ec6d3cd6
Majholl <mohammadreza1231380@gmail.com>
2025-05-23 01:38:13 +0330

    added artists/albums/generes into django admin panel

--- core_apps/musicapp/admin.py
+++ core_apps/musicapp/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 
 from .models.playlists import PlaylistModel
 from .models.musics import MusicModel
+from .models.artists import ArtistsModel
+from .models.albums import AlbumModel
+from .models.genres import GenereModel
+
 
 
 class PlaylistSite(admin.ModelAdmin):
@@ -42,7 +46,6 @@ class PlaylistSite(admin.ModelAdmin):
     
     
     
-    
 class MusicSite(admin.ModelAdmin):
     list_display = ['title', 'created_at']
     
@@ -72,7 +75,94 @@ class MusicSite(admin.ModelAdmin):
     get_artists_data.short_description = 'artist - data'
     
     
+  
+
+  
+class ArtistSite(admin.ModelAdmin):
+    list_display = ['name', 'realname', 'bio']
     
+    list_display_links = list_display
+    
+    list_per_page = 15
+    
+    search_fields = ['artist__name', 'artist__realname', 'artist__bio']
+    
+    list_filter =  ['name', ]
+    
+    readonly_fields = ('get_image_artist', 'created_at', 'updated_at',)
+    
+    fieldsets = (
+        ('Music info', {'fields': ('name', 'realname', 'bio',)},),
+        
+        ('File-Cover', {'fields': ('get_image_artist',),}),
+        
+        ('DateTime', {'fields': ('created_at', 'updated_at',),}),   
+    )
+      
+    def get_image_artist(self, obj):
+        if not obj.image :
+            return None
+         
+        return ','.join([f"{i.name}-{i.image.url} " for i in obj])
+    
+    get_image_artist.short_description = 'Image'
+    
+
+
+  
+class AlbumSite(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    
+    list_display_links = list_display
+    
+    list_per_page = 15
+    
+    search_fields = ['album__title', 'album__artist_id__name', 'album__music_id__title']
+    
+    list_filter =  ['title', 'artist_id__name', 'music_id__title']
+    
+    readonly_fields = ('created_at', 'updated_at',)
+    
+    fieldsets = (
+        ('Music info', {'fields': ('title', 'totaltracks', 'description',)},),
+        
+        ('File-Cover', {'fields': ('cover', ),}),
+        
+        ('DateTime', {'fields': ('created_at', 'updated_at',),}),   
+    )
+
+  
+  
+
+class GenereSite(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    
+    list_display_links = list_display
+    
+    list_per_page = 15
+    
+    search_fields = ['genere__name', 'genere__artist_id__name', 'genere__playlist_id__title', 'genere__music_id__title']
+    
+    list_filter =  ['name', 'artist_id__name', 'music_id__title', 'playlist_id__title']
+    
+    readonly_fields = ('created_at', 'updated_at',)
+    
+    fieldsets = (
+        ('Music info', {'fields': ('name',  'description',)},),
+        
+        ('File-Cover', {'fields': ('cover', ),}),
+        
+        ('DateTime', {'fields': ('created_at', 'updated_at',),}),   
+    )  
+    
+
+
+
+
+
 
 admin.site.register(PlaylistModel, PlaylistSite)
 admin.site.register(MusicModel, MusicSite)
+admin.site.register(ArtistsModel, ArtistSite)
+admin.site.register(AlbumModel, AlbumSite)
+admin.site.register(GenereModel, GenereSite)
